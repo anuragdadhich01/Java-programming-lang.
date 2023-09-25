71619d1a4f6a5afda81a096c17f31a9456234e18
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 17:53:33 +0000

    Star patterns

--- Java Basics/Javabasics.java
+++ Java Basics/Javabasics.java
@@ -1,9 +1,24 @@
 /**
- * Javabasics
- */
+//  * Javabasics
+//  */
+// public class Javabasics {
+// public static void main(String args[]){
+//     System.out.println("1 2 3 4 5\n World");
+//     System.out.println("Anurag Dadhich\n");
+// }
+// }
+
+/**************** PATTERNS ********************/
+
+
 public class Javabasics {
-public static void main(String args[]){
-    System.out.println("1 2 3 4 5\n World");
-    System.out.println("Anurag Dadhich\n");
-}
+   public static void main(String[] args) {
+    System.out.println("******");
+    System.out.println("*****");
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+   }
+    
 }
