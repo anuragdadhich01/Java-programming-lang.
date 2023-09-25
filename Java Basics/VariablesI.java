ff0acf17a78838fbbfb83c49892d396d9d4b4cc2
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 18:03:47 +0000

    Variables

---
+++ Java Basics/VariablesI.java
@@ -0,0 +1,16 @@
+/**
+ * VariablesI
+ */
+public class VariablesI {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Anurag Dadhich";
+        System.out.println(name);
+        a = b;
+        System.out.println(a);
+    }
+}
