579863468afade614720621a50d509b12e385923
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 19:18:11 +0000

    Scanner

---
+++ Java Basics/Input.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+// public class Input {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         // String Input = sc.next();
+//         // System.out.println(Input);
+//         String name = sc.nextLine();
+//         System.out.println(name);
+
+//         float price = sc.nextLine()
+//     }
+// }
+
+
+/*           INPUT in JAVA              
+            
+next
+nextLine
+nextInt
+nextByte
+nextfloat
+nextDouble
+nextBoolean
+nextShort
+nextLong
+*/
+
+//   Sum of a & b 
+
+/**
+ * InnerInput
+ */
+public class Input {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.out.println(sum);
+    }
+}
