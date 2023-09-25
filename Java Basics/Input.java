6959f9819d080dca02976e2300b3788e0aac0792
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 19:22:49 +0000

    Product

--- Java Basics/Input.java
+++ Java Basics/Input.java
@@ -31,13 +31,30 @@ nextLong
 /**
  * InnerInput
  */
+// public class Input {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b ;
+//         System.out.println(sum);
+//     }
+// }
+
+//                  Product 
+
+/**
+ * Input
+ */
 public class Input {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+    
         int b = sc.nextInt();
-        int sum = a + b ;
-        System.out.println(sum);
+        int Product = a * b;
+        System.out.println(Product);
     }
 }
