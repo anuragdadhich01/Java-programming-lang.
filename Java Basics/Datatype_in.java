435670608f7f1c2032bc3e5285a31c17c949d54b
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 18:30:47 +0000

    comments

--- Java Basics/Datatype_in.java
+++ Java Basics/Datatype_in.java
@@ -31,3 +31,26 @@ public class Datatype_in {
         
     }
 }
+
+
+/*              sum in memory    */
+
+/**
+ * InnerDatatype_in
+ */
+public class Datatype_in {
+public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    System.out.println(sum);
+}
+    
+}
+
+//       Comment's for a  single line
+
+/*
+ multi line comments
+
+ */
