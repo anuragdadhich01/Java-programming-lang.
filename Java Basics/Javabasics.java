66bda4063ab04af3b184c8fa2857e4d0aaeb2496
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 17:48:55 +0000

    Hello Worls

---
+++ Java Basics/Javabasics.java
@@ -0,0 +1,9 @@
+/**
+ * Javabasics
+ */
+public class Javabasics {
+public static void main(String args[]){
+    System.out.println("1 2 3 4 5\n World");
+    System.out.println("Anurag Dadhich\n");
+}
+}
