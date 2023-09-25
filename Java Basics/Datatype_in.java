41d4f3787345c78405db5edb14d462aa0ebd8884
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-25 18:20:57 +0000

    Datatypes and datasize

---
+++ Java Basics/Datatype_in.java
@@ -0,0 +1,33 @@
+/*  
+Data types  in Java  
+there are two types of Datatypes
+1. Primitive                                               2. Non-Primitive
+   byte  = 1bytes[-128 to 127]                                  String
+   short = 2bytes[ ]                                            Array
+   char  = 2bytes ['a' to '2','@','%']                          Class
+   boolean = 1bytes[true,false]                                 Object
+   int  = 4bytes [-2b to +2b ]                                  Interface
+   long = 8bytes
+   float = 4bytes
+   double = 8bytes
+
+*/
+
+
+public class Datatype_in {
+    public static void main(String[] args) {
+        // byte b = 8;
+        // System.out.println(b);
+        // char ch = 'a';
+        // System.out.println(ch);
+        // boolean var = false;
+        // System.out.println(var);
+        // float price = 10.5;
+        // System.out.println(price);
+        // int number = 25;
+        // //long
+        // //double
+        // short n  = 240;
+        
+    }
+}
