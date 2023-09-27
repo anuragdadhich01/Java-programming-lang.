f00ab24db78902b934f5c3f232b3dca98967b69b
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-09-27 17:38:29 +0000

    typecasting

--- Java Basics/Datatype_in.java
+++ Java Basics/Datatype_in.java
@@ -1,56 +1,56 @@
-/*  
-Data types  in Java  
-there are two types of Datatypes
-1. Primitive                                               2. Non-Primitive
-   byte  = 1bytes[-128 to 127]                                  String
-   short = 2bytes[ ]                                            Array
-   char  = 2bytes ['a' to '2','@','%']                          Class
-   boolean = 1bytes[true,false]                                 Object
-   int  = 4bytes [-2b to +2b ]                                  Interface
-   long = 8bytes
-   float = 4bytes
-   double = 8bytes
+// /*  
+// Data types  in Java  
+// there are two types of Datatypes
+// 1. Primitive                                               2. Non-Primitive
+//    byte  = 1bytes[-128 to 127]                                  String
+//    short = 2bytes[ ]                                            Array
+//    char  = 2bytes ['a' to '2','@','%']                          Class
+//    boolean = 1bytes[true,false]                                 Object
+//    int  = 4bytes [-2b to +2b ]                                  Interface
+//    long = 8bytes
+//    float = 4bytes
+//    double = 8bytes
 
-*/
+// */
 
 
-public class Datatype_in {
-    public static void main(String[] args) {
-        // byte b = 8;
-        // System.out.println(b);
-        // char ch = 'a';
-        // System.out.println(ch);
-        // boolean var = false;
-        // System.out.println(var);
-        // float price = 10.5;
-        // System.out.println(price);
-        // int number = 25;
-        // //long
-        // //double
-        // short n  = 240;
+// public class Datatype_in {
+//     public static void main(String[] args) {
+//         // byte b = 8;
+//         // System.out.println(b);
+//         // char ch = 'a';
+//         // System.out.println(ch);
+//         // boolean var = false;
+//         // System.out.println(var);
+//         // float price = 10.5;
+//         // System.out.println(price);
+//         // int number = 25;
+//         // //long
+//         // //double
+//         // short n  = 240;
         
-    }
-}
+//     }
+// }
 
 
-/*              sum in memory    */
+// /*              sum in memory    */
 
-/**
- * InnerDatatype_in
- */
-public class Datatype_in {
-public static void main(String[] args) {
-    int a = 10;
-    int b = 5;
-    int sum = a + b;
-    System.out.println(sum);
-}
+// /**
+//  * InnerDatatype_in
+//  */
+// public class Datatype_in {
+// public static void main(String[] args) {
+//     int a = 10;
+//     int b = 5;
+//     int sum = a + b;
+//     System.out.println(sum);
+// }
     
-}
+// }
 
-//       Comment's for a  single line
+// //       Comment's for a  single line
 
-/*
- multi line comments
+// /*
+//  multi line comments
 
- */
+//  */
---
+++ Java Basics/TypeConversion.java
@@ -0,0 +1,43 @@
+// public class TypeConversion {
+//     public static void main(String[] args) {
+//          int a = 25;
+//          long b = a;
+//         //  long a = 25;
+//         // int b = a;
+//     }
+// }
+
+
+// byte -> short -> int -> float -> long -> double
+
+// import java.util.Scanner;
+
+// /**
+//  * TypeConversion
+//  */
+// public class TypeConversion {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         float a = 25.12f;
+//         int b = (int)a;
+//         System.out.println(b);
+//     }
+// }
+
+////////  typecasting
+
+/**
+ * InnerTypeConversion
+ */
+public class TypeConversion {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+        int number = ch;
+        int number = ch2;
+        System.out.println(number);
+        System.out.println(number2);
+    }
+}
+
