b0ff2fb287a3117c78a4f287ead794c1e2d269dc
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-10-03 15:28:14 +0000

    questions

--- Questions.java
+++ Questions.java
@@ -41,7 +41,7 @@ import java.util.Scanner;
 
 
 
- /*
+ /*P
   *Question3:Entercostof3itemsfromtheuser(usingfloatdatatype)-apencil,apenandan eraser. You have to output the total cost of the items back to the user as their bill?
  
   */
