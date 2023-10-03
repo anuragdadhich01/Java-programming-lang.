e68a42e1810c114da2f928b1982c6ceda63a06d0
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-10-03 15:41:44 +0000

    binary

---
+++ Java Basics/Arrthmetic_op.java
@@ -0,0 +1,24 @@
+// Arthmatics Operrators//
+/* There are Two types of Arthmetics overeaters'
+  Binary                       Unary
+  
+  +                             ++    a = a+1 a++ ,++a
+  -                             --    a = a-1 a-- ,--a
+  *
+  /
+  % (Modulo):- a%b = 10%5=0 Remender provide karta hai
+
+
+*/ 
+
+/**
+ * Arrthmetic_op
+ */
+public class Arrthmetic_op {
+
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("Modulo =" + (A%B));
+    }
+}
