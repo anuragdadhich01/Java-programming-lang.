1bfdcdc894e83da281c7af00a659962f86b2e79c
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-10-03 15:59:33 +0000

    relational

---
+++ Java Basics/relational_operators.java
@@ -0,0 +1,23 @@
+/*           
+ 
+         RELATIONAL OPERATORS
+
+    ==   equal to
+    !=  not equal to
+    >
+    <
+    >=
+    <=
+ */
+
+ /**
+  * relational_operators
+  */
+ public class relational_operators {
+ 
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 10;
+        System.out.println( A != B);
+    }
+ }
