5c0ffad629326744a4181eff9ce331faf8bfa16b
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-10-05 15:32:36 +0000

    op

---
+++ Java Basics/Assignment_op.java
@@ -0,0 +1,27 @@
+/*                       
+ Assignment operaters 
+
+ =     
+
+ +=
+
+ -=
+
+ *=
+ 
+ /=
+ */
+
+
+
+
+
+
+
+
+public class Assignment_op {
+    public static void main(String[] args) {
+        int A = 10;
+        A +=10;
+    System.out.println(A);   }
+}
