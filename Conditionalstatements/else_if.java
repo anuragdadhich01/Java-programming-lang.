4c8fde54b2b487c88dc083f94ac7183528520e55
Anurag Dadhich <120323692+anuragdadhich01@users.noreply.github.com>
2023-10-08 07:37:22 +0000

    1

---
+++ Conditionalstatements/else_if.java
@@ -0,0 +1,12 @@
+/**
+ * else_if
+ */
+public class else_if {
+
+    public static void main(String[] args) {
+        int age = 13;
+        if(age >= 18){
+            System.out.println("Adult");
+        }
+    }
+}
